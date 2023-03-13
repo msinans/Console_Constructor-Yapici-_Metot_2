d9117b055a2ffea10bc2ebe3081ac2f2291c5360
m.sinans <msomercikoglu@gmail.com>
2023-03-13 14:25:05 +0300

    Add project files.

---
+++ Console_Ornek_6_Constructor(Yapici)_Metot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Console_Constructor_Yapici_Metot_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci("Ali - Yeşilay");
+            Console.Read();
+        }
+    }
+}
